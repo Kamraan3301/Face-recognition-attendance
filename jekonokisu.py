86e762fb3ed239735311af794f8e329e63b7fc19
kamraan3301 <kamran15-7039@diu.edu.bd>
2020-12-12 20:56:51 +0600

    ml-project

--- jekonokisu.py
+++ jekonokisu.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template,request,session,logging,url_for,redirect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+from passlib.hash import sha256_crypt
+
 from flask import render_template
 from flask import Response
 
@@ -9,24 +13,51 @@ from imutils.video import VideoStream
 import imutils
 
 app = Flask(__name__)
-videoStream = VideoStream(src=0).start()
+videoStream = VideoStream(src=0).start()#for mobile cam
+videoStream1 = VideoStream(src=1).start() #web or laptop.
+
+import numpy as np
+import cv2
+
+
 
 @app.route("/")
 def index():
-    return render_template("videostream.html")
+    return render_template("stream.html")
+
+
+
+
+
 
 @app.route("/video")
 def video():
     return Response(generateFrames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 def generateFrames():
     while True:
         frame = videoStream.read()
         frame = imutils.resize(frame, width=600)
         (flag, encodedImage) = cv2.imencode(".jpg", frame)
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n') 
+
+
+
+
+# ------------------------------------------Second Camera------------------------------------------------
+@app.route("/video1")
+def video1():
+    return Response(generateFrames1(), mimetype='multipart/x-mixed-replace; boundary=frame')
+def generateFrames1():
+    while True:
+     frame1 = videoStream1.read()
+     frame1 = imutils.resize(frame1, width=800)
+     (flag, encodedImage) = cv2.imencode(".jpg", frame1)
+     yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+
+
+       
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
-
